f4ecb9c8f46b882c4632b22007f184115b6ff400
noname-03 <fahrurrozi.arul3@gmail.com>
2021-08-17 15:11:59 +0700

    view home user

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -8,11 +8,11 @@ use Alert;
 
 class HomepageController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
     public function index() {
         $barangs = Barang::paginate(20); //membatasi jumlah barang yang akan ditampilkan
         return view('homepage.index', compact('barangs')); //menampilkan barang ke halaman home
